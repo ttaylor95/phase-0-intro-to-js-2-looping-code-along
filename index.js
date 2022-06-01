98bc9ccd843ca366b740e3ba05fc84ebfbddf1be
Teanna Taylor <teannamtaylor95@gmail.com>
2022-06-01 14:04:01 -0700

    commit tree e6d5867923f9b48dbd93e28fbace118371b1abec

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+// function writeCards that accepts an array and event name as arguments
+
+function writeCards(names,event){
+    const cards = [];
+    for(let i=0; i < 3; i++) {
+        cards.push(`Thank you, ${names[i]}, fopr the wonderful ${event} gift!`);
+
+
+    }
+
+return cards
+
+}
+
+writeCards(["Guadalupe", "Ollie","Aki"], "surprise");
